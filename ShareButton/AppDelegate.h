9c7d1862c66c520cbb49ed82bdeb2d7101130c8a
LuQuan Intrepid <lu@intrepid.io>
2015-11-15 17:34:36 -0500

    Initial Commit

---
+++ ShareButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareButton
+//
+//  Created by LuQuan Intrepid on 11/15/15.
+//  Copyright © 2015 Lu Quan Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShareButton
+//
+//  Created by LuQuan Intrepid on 11/15/15.
+//  Copyright © 2015 Lu Quan Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
