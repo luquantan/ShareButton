8665450a766846302c8d82f8fd7088c256761ba7
LuQuan Intrepid <lu@intrepid.io>
2015-11-15 18:40:37 -0500

    Implement slidingView

--- ShareButton/View/LQSlidingView.m
+++ ShareButton/View/LQSlidingView.m
@@ -7,22 +7,62 @@
 //
 
 #import "LQSlidingView.h"
-#import "UIView+loadNib.h"
+#import "UIView+constraints.h"
+
+@interface LQSlidingView()
+@property (nonatomic, strong) UIView *nibView;
+@property (weak, nonatomic) IBOutlet UIView *slidingView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *slidingViewTrailingConstraint;
+@end
 
 @implementation LQSlidingView
 
-- (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        [self loadNib];
-    }
-    return self;
-}
-
 - (void)awakeFromNib {
     [super awakeFromNib];
+    [self loadNib];
+    [self setupCorner];
+    [self setupTapGesture];
+}
+
+#pragma mark - Setup
+
+- (void)setupCorner {
     self.layer.cornerRadius = CGRectGetHeight(self.frame) / 2;
     self.layer.masksToBounds = YES;
+
+    self.slidingView.layer.cornerRadius = CGRectGetHeight(self.frame) / 2;
+    self.slidingView.layer.masksToBounds = YES;
+}
+
+- (void)setupTapGesture {
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapView)];
+    [self addGestureRecognizer:tap];
+}
+
+#pragma mark - Gesture
+
+- (void)didTapView {
+    if (CATransform3DEqualToTransform(self.slidingView.layer.transform, CATransform3DIdentity)) {
+        [UIView animateWithDuration:0.5 animations:^{
+            self.slidingView.transform = CGAffineTransformMakeTranslation(-CGRectGetWidth(self.slidingView.bounds), 0.0f);
+        }];
+    } else {
+        [UIView animateWithDuration:0.5 animations:^{
+            self.slidingView.transform = CGAffineTransformIdentity;
+        }];
+    }
+}
+
+#pragma mark - Load Nib
+
+- (void)loadNib {
+    if (self) {
+        self.nibView = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class])
+                                                      owner:self
+                                                    options:nil] lastObject];
+        [self addSubview:self.nibView];
+        [self constrainViewToAllEdges:self.nibView];
+    }
 }
 
 @end
